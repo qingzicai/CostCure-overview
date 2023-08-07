b14cce9d8679f1fd58283942c3ae49b66cf2a9ea
ivy <dllmwl08@gmail.com>
2023-08-07 12:04:41 -0400

    tiny change on file directory

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -8,7 +8,7 @@ function LoginPage() {
         <section className="flex flex-col justify-center items-center md:flex-row h-screen ">
             <div className="bg-blue-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
                 <img
-                    src="src/assets/women-illustration.jpg"
+                    src="../assets/women-illustration.jpg"
                     alt="women illustration" className="w-full h-full object-cover"/>
             </div>
             <div
